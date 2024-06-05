3eb6b2a1b37a4253956fb8fb66d5fb31c7e8aee2
LeeNorrell <156039272+LeeNorrell@users.noreply.github.com>
2024-06-04 23:00:20 -0500

    Setting up how i would like the code to be structured and read through. needs work with the pages and styling as the next step

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import Nav from './Nav';
+export default function Header() {
+    return (
+        <header>
+            <h1>Lee Norrell <Nav /> </h1>
+        </header>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import Home from './Home';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import ErrorPage from './pages/ErrorPage';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<RouterProvider router={router} />
 )
